6836fd8f5921a578aa8f5289315061aa3b2d26b2
Andris Zalitis <andris@Andriss-MacBook-Pro.local>
2015-01-08 20:01:42 +0200

    Initial Commit

---
+++ TestCreditCardsAPI/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestCreditCardsAPI
+//
+//  Created by Andris Zalitis on 08/01/15.
+//  Copyright (c) 2015 Plannit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
